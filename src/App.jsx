b22ea52ccc8f3d579d94e8004aaaaa5f234844d6
JJFAO <jjfalonso@gmail.com>
2021-07-13 02:44:48 -0300

    style(cart): add animation to cart and color transitions

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./components/Header";
 import Products from "./components/Products";
 import './App.css'
+import './animation.css'
 
 function App() {
   return (
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -20,10 +20,10 @@ export default function Products() {
       <div className="d-flex flex-wrap justify-content-center products-cards">
         {products.map((product) => (
           <div key={product.id} className="card m-2">
-              <img src={product.image} alt="" className="img-fit p-4" />
+            <img src={product.image} alt="" className="img-fit p-4" />
             <div className="card-body text-center">
               <h5 className="card-title">{product.title.slice(0, 20)}</h5>
-              <button className="btn btn-outline-success">
+              <button className="btn border-success btn-cart pulsating-circle">
                 <img
                   src="https://icongr.am/material/cart.svg?size=30&color=2ca049"
                   alt=""
