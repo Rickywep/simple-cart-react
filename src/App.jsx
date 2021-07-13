296d897aa86e39117db298313731115d687d7d29
Ricky Moreno <ricky.gnr.moreno@gmail.com>
2021-07-13 00:29:54 -0300

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import Header from "./components/Header";
 import Products from "./components/Products";
 
 function App() {
   return (
     <div>
-      <h1>Tiendita</h1>
+      <Header />
       <Products />
     </div>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,18 +2,12 @@ import React from "react";
 
 export default function Header() {
   return (
-    <nav class="nav">
-      <a class="nav-link active" aria-current="page" href="#">
-        Active
+    <nav className="nav bg-dark">
+      <a className="nav-link active" aria-current="page" href="#">
+        Tiendita
       </a>
-      <a class="nav-link" href="#">
-        Link
-      </a>
-      <a class="nav-link" href="#">
-        Link
-      </a>
-      <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">
-        Disabled
+      <a className="nav-link" href="#">
+        cart
       </a>
     </nav>
   );
